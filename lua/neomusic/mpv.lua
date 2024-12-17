81aa25dd56501bfd96cb96234320af9216e0c48b
aroario2003 <borkbork1031@gmail.com>
2024-12-17 10:01:48 -0500

    feat: Get song duration for internal purpose...missed a file on last commit

--- lua/neomusic/mpv.lua
+++ lua/neomusic/mpv.lua
@@ -1,5 +1,6 @@
 local M = {mpv_pid = nil}
 
+---Play a song with mpv
 function M._internal_play_song(song_path)
     local nm_sock = require("neomusic.socket")
     local nm_win = require("neomusic.window")
@@ -17,16 +18,19 @@ function M._internal_play_song(song_path)
     M.mpv_pid = vim.fn.jobpid(job_id)
 end
 
+---Pause a song
 function M._internal_pause_song()
     local nm_sock = require("neomusic.socket")
     nm_sock._write('{ \"command\": [\"set_property\", \"pause\", true] }')
 end
 
+---Unpause a song 
 function M._internal_unpause_song()
     local nm_sock = require("neomusic.socket")
     nm_sock._write('{ \"command\": [\"set_property\", \"pause\", false] }')
 end
 
+---Kill the mpv process if it is still running
 function M._kill_mpv()
     local nm_win = require("neomusic.window")
    if not M.mpv_pid then
@@ -37,6 +41,25 @@ function M._kill_mpv()
     if not success then
        nm_win.notification("Something went wrong trying to kill mpv process with pid: %d", M.mpv_pid)
     end
+end
+
+---Get the song duration by using the mpv socket
+---@return number
+function M._get_song_duration()
+    local nm_sock = require("neomusic.socket")
+    local ret = nm_sock._write('{\"command\": [\"get_property\", \"duration\"]}')
+    local split = string.gmatch(ret[1], "([^"..",".."]+)")
+    local data_parts = {}
+    for item in split do
+        if string.find(item, "data") then
+            local data = string.gmatch(item, "([^"..":".."]+)")
+            for section in data do
+               table.insert(data_parts, section)
+            end
+            break
+        end
+    end
+    return tonumber(data_parts[2], 10)
 end
 
 return M
